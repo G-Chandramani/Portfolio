3f02b370dd490d3bb66c9584193fe44ef527828c
G-Chandramani <gajbharechandramani@gmail.com>
2026-01-22 12:53:50 +0530

    update for vertex

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,46 @@
 import { BrowserRouter } from "react-router-dom";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
-import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
+import {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+  StarsCanvas,
+} from "./components";
 import ErrorBoundary from "./components/ErrorBoundary";
 
 const App = () => {
   return (
     <ErrorBoundary>
       <BrowserRouter>
-        <div className='relative z-0 bg-primary'>
-          <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+        <div className="relative z-0 bg-primary">
+          <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
             <Navbar />
             <Hero />
           </div>
+
           <About />
           <Experience />
           <Tech />
           <Works />
           <Feedbacks />
-          <div className='relative z-0'>
+
+          <div className="relative z-0">
             <Contact />
             <StarsCanvas />
           </div>
+
+          {/* Vercel Speed Insights */}
+          <SpeedInsights />
         </div>
       </BrowserRouter>
     </ErrorBoundary>
   );
-}
+};
 
 export default App;
